cd43160ed156602d61a8a762a320a62a6fb32dbb
Kevin Phan Le <kevinphanle11@gmail.com>
2025-02-22 02:26:14 -0800

    resolve typescript error when building

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -3,9 +3,10 @@ import skillsData from "@/data/skills.json";
 import { skillsSchema } from "@/lib/schemas";
 import SkillItem from "./SkillItem";
 
-// type SkillCategory = keyof typeof skills;
+type SkillCategory = keyof typeof skillsData.skills;
+
 const Skills: React.FC = () => {
-  const skills = skillsSchema.parse(skillsData);
+  const skills = skillsSchema.parse(skillsData).skills;
   console.log(skills);
 
   return (
@@ -16,8 +17,8 @@ const Skills: React.FC = () => {
       <h2 className="mb-1 text-2xl text-slate-800 dark:text-slate-200 italic font-semibold">
         skills
       </h2>
-      {Object.keys(skills.skills).map((key, index) => (
-        <SkillItem key={index} title={key} skill={skills.skills[key]} />
+      {(Object.keys(skills) as SkillCategory[]).map((key, index) => (
+        <SkillItem key={index} title={key} skill={skills[key]} />
       ))}
     </div>
   );
