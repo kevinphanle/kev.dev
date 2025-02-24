a0a708009c3084893aba93b56d5951d98727b0e2
Kevin Phan Le <kevinphanle11@gmail.com>
2025-02-23 21:59:49 -0800

    increase min height and width for download resume button

--- src/app/components/actionBar.tsx
+++ src/app/components/actionBar.tsx
@@ -7,10 +7,10 @@ import Socials from "./Socials";
 export const ActionBar = () => {
   return (
     <section className="flex items-center mb-10">
-      <Button className="mr-6">
+      <Button className="mr-6" aria-label="Download Resume">
         <Link
           href="/Resume 2024.pdf"
-          className="flex items-center gap-1 tracking-[0.5px] text-[16px]"
+          className="flex items-center gap-1 tracking-[0.5px] min-h-[48px] min-w-[48px]"
           target="_blank"
         >
           <FileDown size={16} />
